c53b3c3dd87d3afb6201647919d489e1862c719a
yanou16 <rayanelouzazna@gmail.com>
2024-12-23 14:59:07 +0100

    Fix unescaped single quote in Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -22,7 +22,7 @@ const Contact = () => {
 </div>
 
 <div className='bg-white/10 p-6 rounded-xl max-w-[550px]'>
-<h2 className='text-5xl font-bold text-orange-400 mb-4'>Let's connect</h2>
+<h2 className='text-5xl font-bold text-orange-400 mb-4'>Let&apos;s connect</h2>
 <p className='text-white/70 mb-6'>Send Me a Message to work !!!</p>
 <form className='space-y-4' action="https://getform.io/f/bvrrlzlb" method='POST'>
 <div className='grid md:grid-cols-2 gap-4'>
